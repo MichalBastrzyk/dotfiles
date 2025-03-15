9442eec1505ab8f1d7d9afc868bc633a9628a844
Michał Bastrzyk <michal.bastrzyk.pl@gmail.com>
2025-03-15 21:50:42 +0100

    feat: Add copy to clipboard keybinds

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,6 +1,4 @@
--- Options are automatically loaded before lazy.nvim startup
--- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
--- Add any additional options here
+vim.g.mapleader = ' '
 
 vim.opt.clipboard = ""
 vim.opt.number = true
@@ -54,3 +52,10 @@ vim.opt.signcolumn = "yes:1"
 
 vim.opt.spelllang = "en_us"
 vim.opt.spell = true
+
+-- Copy to clipboard
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]], { desc = "Yank to system clipboard" })
+vim.keymap.set("n", "<leader>Y", [["+Y]], { desc = "Yank whole line to system clipboard" })
+
+vim.keymap.set({ "n", "v" }, "<leader>P", [["+P"]], { desc = "Paste from system clipboard" })
+vim.keymap.set({ "n", "v" }, "<leader>p", [["+p"]], { desc = "Paste from system clipboard" })
