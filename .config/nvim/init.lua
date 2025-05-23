fff1ec1059beec15431eeff3a68b87b999f3389d
Michał Bastrzyk <michal.bastrzyk.pl@gmail.com>
2025-05-23 18:39:34 +0200

    neovim: Added oil.nvim and updated deps

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -153,6 +153,14 @@ vim.opt.rtp:prepend(lazypath)
 require("lazy").setup({
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
   "tpope/vim-sleuth", -- Detect tabstop and shiftwidth automatically
+  {
+    "stevearc/oil.nvim",
+    ---@module 'oil'
+    ---@type oil.SetupOpts
+    opts = {},
+    dependencies = { { "echasnovski/mini.icons", opts = {} } },
+    lazy = false,
+  },
 
   -- NOTE: Plugins can also be added by using a table,
   -- with the first argument being the link and the following
